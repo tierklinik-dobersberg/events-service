1d50d6e88b43746f07a6da27993918abf8c2d768
Patrick Pacher <patrick.pacher@gmail.com>
2024-08-28 17:10:28 +0200

    Update logging middleware

--- cmds/events-service/main.go
+++ cmds/events-service/main.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 
 	connect "github.com/bufbuild/connect-go"
 	"github.com/bufbuild/protovalidate-go"
@@ -92,7 +93,11 @@ func main() {
 
 	loggingHandler := func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+
 			next.ServeHTTP(w, r)
+
+			slog.Info("handled request", slog.Any("method", r.Method), slog.Any("path", r.URL.Path), slog.Any("duration", time.Since(start).String()))
 		})
 	}
 
