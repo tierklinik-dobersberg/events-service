99ccbe3ab14173e5284729c90aea1abe796f9be9
Patrick Pacher <patrick.pacher@gmail.com>
2026-05-18 10:41:58 +0200

    fix(automation/provet): use reflection to try to convert input parameters for api calls

--- internal/automation/modules/provet/provet.go
+++ internal/automation/modules/provet/provet.go
@@ -101,7 +101,12 @@ func createMethod(vu modules.VU, obj *goja.Object, client *provet.ProvetClient, 
 				callArgs = append(callArgs, reflect.ValueOf(args[argIdx].Export()))
 				continue
 			} else {
-				vu.Log().Log(context.Background(), slog.LevelInfo, "cannot directly assign parameter value", "expected", inputParam.String(), "expectedKind", inputParam.Kind().String(), "got", exportType.String(), "gotKind", exportType.Kind().String())
+				if exportType.ConvertibleTo(inputParam) {
+					callArgs = append(callArgs, reflect.ValueOf(args[argIdx]).Convert(inputParam))
+					continue
+				}
+
+				vu.Log().Log(context.Background(), slog.LevelInfo, "cannot directly assign/convert parameter value", "expected", inputParam.String(), "expectedKind", inputParam.Kind().String(), "got", exportType.String(), "gotKind", exportType.Kind().String())
 			}
 
 			wasPointer := false
