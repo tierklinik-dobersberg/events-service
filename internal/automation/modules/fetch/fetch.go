b08d0e3378d4e548ad97771102f309910bc3f12c
Patrick Pacher <patrick.pacher@gmail.com>
2026-05-19 09:37:19 +0200

    wip(automation/fetch): add custom fetch implementation

--- internal/automation/modules/fetch/fetch.go
+++ internal/automation/modules/fetch/fetch.go
@@ -1,9 +1,14 @@
 package fetch
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
 	"github.com/dop251/goja"
-	"github.com/elazarl/goproxy"
-	"github.com/olebedev/gojax/fetch"
+	"github.com/tierklinik-dobersberg/events-service/internal/automation/common"
 	"github.com/tierklinik-dobersberg/events-service/internal/automation/modules"
 )
 
@@ -12,9 +17,67 @@ type Module struct{}
 func (*Module) Name() string { return "fetch" }
 
 func (*Module) NewModuleInstance(vu modules.VU) (*goja.Object, error) {
-	if err := fetch.Enable(vu.EventLoop(), goproxy.NewProxyHttpServer()); err != nil {
-		return nil, err
-	}
+	vm := vu.Runtime()
+
+	vm.Set("fetch", func(url string, options map[string]any) goja.Value {
+		method := "GET"
+		if m, ok := options["method"].(string); ok && m != "" {
+			method = m
+		}
+
+		var body io.Reader
+		if b, ok := options["body"].(string); ok {
+			body = strings.NewReader(b)
+		}
+
+		req, err := http.NewRequest(method, url, body)
+		if err != nil {
+			common.Throw(vm, err)
+		}
+
+		promise, resolve, reject := vm.NewPromise()
+
+		go func() {
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				reject(err)
+				return
+			}
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				reject(fmt.Errorf("failed to read response body: %w", err))
+				return
+			}
+
+			// prepare response headers
+			resHeaders := map[string]any{}
+			for k, v := range resp.Header {
+				resHeaders[k] = v
+			}
+
+			resolve(map[string]any{
+				"ok":         resp.StatusCode >= 200 && resp.StatusCode < 300,
+				"status":     resp.StatusCode,
+				"statusText": resp.Status,
+				"url":        resp.Request.URL.String(),
+				"headers":    resHeaders,
+				"text":       func() string { return string(body) },
+				"json": func() (any, error) {
+					var data any
+					err := json.Unmarshal(body, &data)
+					return data, err
+				},
+				"arrayBuffer": func() any { return vm.NewArrayBuffer(body) },
+				"bytes": func() (any, error) {
+					return vm.New(vm.Get("Uint8Array"), vm.ToValue(vm.NewArrayBuffer(body)))
+				},
+			})
+		}()
+
+		return vm.ToValue(promise)
+	})
 
 	return nil, nil
 }
